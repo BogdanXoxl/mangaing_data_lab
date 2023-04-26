305c7ce8e2257ddbb78d885911b022fbe4f2bafb
BogdanXoxl <bogxoxl39@gmail.com>
2023-04-26 09:56:00 +0300

    test: 💍 env var

--- src/components/Providers/ApolloClient.ts
+++ src/components/Providers/ApolloClient.ts
@@ -2,15 +2,11 @@ import type { NormalizedCacheObject } from "@apollo/client";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 let client: ApolloClient<NormalizedCacheObject> | null = null;
-let host: string | null | undefined = null;
 
 export const getClient = () => {
-  // eslint-disable-next-line no-console
-  console.log("here>> ", host);
   if (!client || typeof window === "undefined") {
-    host = process.env.HOST;
     client = new ApolloClient({
-      uri: `${process.env.HOST}/api/graphql`,
+      uri: `${process.env.NEXT_PUBLIC_VERCEL_URL}/api/graphql`,
       cache: new InMemoryCache(),
     });
   }
